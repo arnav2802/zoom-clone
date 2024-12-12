eeb130890f26c02cc197ea909611167721accc94
arnav2802 <arnav171103@gmail.com>
2024-12-12 13:39:59 +0530

    working on home screen

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,22 @@ import React from 'react'
 const Home = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
-    <h1 className='text-3xl font-bold'> HOME </h1></section>
+      <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
+          <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+              <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-basee font-normal'>
+                  Upcoming Meeting at: 12:30 PM
+              </h2>
+              <div className='flex flex-col gap-2'>
+                  <h1 className='text-4xl font-extrabold lg:text-7xl'>
+                      11:30 AM
+                  </h1>
+                  <p className='text-sky-1 text-lg font-medium lg:text-2xl'> Saturday - December 13, 2024</p>
+
+              </div>
+
+          </div>
+      </div>
+    </section>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {ClerkProvider} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,26 @@ export default function RootLayout({
                                    }: Readonly<{ children: ReactNode }>) {
   return (
       <html lang="en">
+      <ClerkProvider
+      appearance={{
+          layout:{
+              logoImageUrl:'/icons/yoom-logo.png',
+              socialButtonsVariant: 'iconButton'
+          },
+          variables:{
+              colorText: '#fff',
+              colorPrimary: '#0E78F9',
+              colorBackground: '#1c1f2e',
+              colorInputBackground: '#252a41',
+              colorInputText: '#fff'
+          }
+      }}>
         <body className={`${inter.className} bg-dark-2`}>
+
 
         {children}
         </body>
+      </ClerkProvider>
 
       </html>
   );
